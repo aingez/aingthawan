07e6540c1445c2f44140894acb0cf986229f4984
aingez <s6401012610042@email.kmutnb.ac.th>
2024-04-10 16:02:47 +0700

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,19 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterContainer = styled.footer`
-  background-color: #333;
-  color: #fff;
+  background-color: #3333331f;
+  color: #000;
   padding: 1rem;
-  text-align: center;
+  display: flex;
+  justify-content: space-between;
   position: absolute;
   bottom: 0;
   width: 100%;
 `;
 
+const LeftText = styled.div`
+  text-align: left;
+`;
+
+const RightText = styled.div`
+  text-align: right;
+  padding-right: 2rem;
+  font-size: 10px;
+`;
+
 const Footer = () => {
   return (
     <FooterContainer>
-      <p>&copy; 2023 Your Portfolio. All rights reserved.</p>
+      <LeftText>
+      </LeftText>
+      <RightText>
+        <p>"To see the world, things dangerous to come to, to see behind walls, draw closer, to find each other, and to feel. That is the purpose of life"</p>
+        <p>- Henry Luce</p>
+        <p>&copy; 2024 Aingthawan K. All rights reserved.</p>
+      </RightText>
     </FooterContainer>
   );
 };
